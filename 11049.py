453112fad56c5c7a1061a92caaa17d73a7bf73e9
kyh0726 <younghyun75398@gmail.com>
2025-09-12 13:41:26 +0900

    feat: dp 풀이 ( 답 봄 )

---
+++ 11049.py
@@ -0,0 +1,24 @@
+import sys
+N = int(sys.stdin.readline().strip())
+arr = []
+for i in range(N):
+    a, b = map(int, sys.stdin.readline().strip().split())
+    arr.append((a,b))
+
+dp = [[0] * N for _ in range(N)]
+
+for term in range(1, N):
+    for start in range(N):
+        if start + term == N:
+            break
+        dp[start][start+term] = int(1e9)
+
+        # t는 start와 start + term의 사이
+        for t in range(start, start + term):
+            # 이게 뭔 소리지
+            # start부터 start + term 지점의 값을 기존 값과 
+            # arr[start][0] * arr[t][1] * arr[start+term][1]은 t 지점에서 나눈 행렬을 다시 곱하는데 드는 비용이다.
+            dp[start][start+term] = min(dp[start][start+term],
+                                         dp[start][t] + dp[t+1][start+term] + arr[start][0] * arr[t][1] * arr[start+term][1])
+
+print(dp[0][N-1])
