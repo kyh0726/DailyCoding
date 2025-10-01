68aea653d58f4845e1942c53da71ceaa9fa1673b
younghyun753 <younghyun75398@gmail.com>
2025-10-01 13:51:01 +0900

    [Gold IV] Title: 타일 채우기, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Gold/2133. 타일 채우기/타일 채우기.py
@@ -0,0 +1,16 @@
+import sys
+
+N = int(sys.stdin.readline().strip())
+dp = [0] * (N+1)
+
+if N % 2 == 1:
+    print(0)
+else:
+    dp[2] = 3
+    for i in range(4, N+1, 2):
+        dp[i] += dp[i-2] * 3 + 2
+        for j in range(2, i-2, 2):
+            dp[i] += dp[j] * 2
+
+
+    print(dp[N])
