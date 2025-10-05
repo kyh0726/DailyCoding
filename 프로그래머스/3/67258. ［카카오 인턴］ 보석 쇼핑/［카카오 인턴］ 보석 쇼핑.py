94b37d5bdc1215fa1cb5153de73717d8526c671e
younghyun753 <younghyun75398@gmail.com>
2025-10-06 02:17:41 +0900

    [level 3] Title: [카카오 인턴] 보석 쇼핑, Time: 61.97 ms, Memory: 16.2 MB -BaekjoonHub

--- 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
+++ 프로그래머스/3/67258. ［카카오 인턴］ 보석 쇼핑/［카카오 인턴］ 보석 쇼핑.py
@@ -1,45 +1,46 @@
-from collections import deque, defaultdict
+from collections import defaultdict
 def solution(gems):
     answer = []
-    min_val = 100001
-    result = deque()
-    gems = deque(gems)
-    num = len(set(gems))
-    collected_gems = set()
-    num_count_dict = defaultdict(int)
-    idx = 0
-    while gems:
+    max_gem_num = len(set(gems))
+    gem_count = defaultdict(int)
+    
+    left = 0
+    right = -1
+    count = 0
+    N = len(gems)
+    min_set = [1,10000000000]
+    
+    while left < N and right < N:
+        if count ==  max_gem_num:
+            min_gap = min_set[1] - min_set[0]
+            if right - left < min_gap:
+                min_set = [left, right]
+            elif right - left == min_gap and left < min_set[0]:
+                min_set = [left, right]
+                
+            
         
-        # 만약 처음 result에 값을 넣는거라면 모든 종류의 gem이 들어가도록 result 값을 채워준다.
-        if not result:
-            while gems:
-                idx += 1
-                pop_val = gems.popleft()
-                result.append(pop_val)
-                collected_gems.add(pop_val)
-                num_count_dict[pop_val] += 1
-                if len(collected_gems) == num:
-                    break
-        else:
-            idx += 1
-            pop_val = gems.popleft()
         
-            result.append(pop_val)
-            num_count_dict[pop_val] += 1
         
-        while result:
-            if num_count_dict[result[0]] >= 2:
-                num_count_dict[result[0]] -= 1
-                result.popleft()
-            else:
+        if count < max_gem_num:
+            right += 1
+            if right == N:
                 break
+            gem = gems[right]
+            gem_count[gem] += 1
+            if gem_count[gem] == 1:
+                count += 1
         
+        else:
+            gem = gems[left]
+            left += 1
+            gem_count[gem] -= 1
+            if gem_count[gem] == 0:
+                count -= 1
         
-        if len(result) < min_val:
-            min_val = len(result)
-            answer = [idx - len(result) + 1,idx]
         
         
     
+        
     
-    return answer
+    return [min_set[0] + 1, min_set[1] + 1]
