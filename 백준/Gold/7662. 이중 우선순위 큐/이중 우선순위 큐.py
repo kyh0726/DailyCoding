6a4c3b32adfea856c7280e718d23861215f04ca1
younghyun753 <younghyun75398@gmail.com>
2026-03-21 12:08:00 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 5104 ms, Memory: 183456 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.py
@@ -1,46 +1,47 @@
 import sys
+import heapq
 from collections import defaultdict
-from heapq import heapify, heappop, heappush
-T = int(sys.stdin.readline().strip())  # 테스트 케이스 개수
+
+T = int(sys.stdin.readline().strip())
+
 
 for _ in range(T):
-    N = int(sys.stdin.readline().strip())  # 연산 개수
-    commands = [sys.stdin.readline().strip() for _ in range(N)]  # N개 명령어 읽기
-    max_heap = []
+    k = int(sys.stdin.readline().strip())
+    
     min_heap = []
-    exist = defaultdict(int)
-    isExist = False
-    for command in commands:
-        action, val = command.split(' ')
-        val = int(val)
-        if action == "I":
-            heappush(max_heap, -val)
-            heappush(min_heap, val)
-            exist[val] += 1
+    max_heap = []
+    visited = defaultdict(int)
 
+    for __ in range(k):
+        cmd, target = sys.stdin.readline().strip().split()
+        target = int(target)
+        if cmd == "I":
+            heapq.heappush(min_heap, target)
+            heapq.heappush(max_heap, -target)
+            visited[target] += 1
         else:
-            if not min_heap or not max_heap:
-                continue
-            if val == 1:
-                while max_heap and exist[-max_heap[0]] == 0:
-                    heappop(max_heap)
-                if max_heap and exist[-max_heap[0]] > 0:
-                    pop_val = -heappop(max_heap)
-                    exist[pop_val] -= 1
+            if target == 1:
+                while max_heap and visited[-max_heap[0]] == 0:
+                    heapq.heappop(max_heap)
+                if max_heap:
+                    pop_node = -heapq.heappop(max_heap)
+                    visited[pop_node] -= 1
+            else:
+                while min_heap and visited[min_heap[0]] == 0:
+                    heapq.heappop(min_heap)
+                if min_heap:
+                    pop_node = heapq.heappop(min_heap)
+                    visited[pop_node] -= 1
+    
 
-            if val == -1:
-                while min_heap and exist[min_heap[0]] == 0:
-                    heappop(min_heap)
-                if min_heap and exist[min_heap[0]] > 0:
-                    pop_val = heappop(min_heap)
-                    exist[pop_val] -= 1
+    while min_heap and visited[min_heap[0]] == 0:
+        heapq.heappop(min_heap)
+    while max_heap and visited[-max_heap[0]] == 0:
+        heapq.heappop(max_heap)
 
-    while min_heap and exist[min_heap[0]] == 0:
-        heappop(min_heap)
-    while max_heap and exist[-max_heap[0]] == 0:
-        heappop(max_heap)
-
-    if min_heap and max_heap:
-        print(f"{-max_heap[0]} {min_heap[0]}")  # 확인용 출력
+        
+    if len(min_heap) == 0 or len(max_heap) == 0:
+        print("EMPTY")
     else:
-        print('EMPTY')
+        print(-heapq.heappop(max_heap), heapq.heappop(min_heap))
+
