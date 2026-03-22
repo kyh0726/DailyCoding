e3581a9e00057e0aae4a8945f254b7fb1ebf30ad
younghyun753 <younghyun75398@gmail.com>
2026-03-22 12:31:05 +0900

    [Gold III] Title: 여왕벌, Time: 3756 ms, Memory: 36216 KB, Score: 83 point -BaekjoonHub

---
+++ 백준/Gold/10836. 여왕벌/여왕벌.py
@@ -0,0 +1,39 @@
+import sys
+
+
+M,N = map(int, sys.stdin.readline().strip().split())
+
+board = [[1] * M for _ in range(M)]
+
+
+
+for _ in range(N):
+    increase_nums = list(map(int, sys.stdin.readline().strip().split()))
+
+    idx = 0
+
+    for row in range(M-1,-1,-1):
+        while increase_nums[idx] == 0:
+            idx += 1
+
+        board[row][0] += idx
+        increase_nums[idx] -= 1
+    
+    for col in range(1, M):
+        while increase_nums[idx] == 0:
+            idx += 1
+
+        board[0][col] += idx
+        increase_nums[idx] -= 1
+    
+
+for i in range(1,M):
+    for j in range(1,M):
+        board[i][j] = max(board[i-1][j-1], board[i][j-1], board[i-1][j])
+
+
+for row in board:
+    for val in row:
+        print(val, end=" ")
+    
+    print('')
